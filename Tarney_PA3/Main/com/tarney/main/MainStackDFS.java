c2b4a088099368ad6a78f9be7b2bdd52187a94a4
1amBulletproof <brandon.tarney@gmail.com>
2016-04-30 15:19:52 -0400

    Main class and hierarchy setup

---
+++ Tarney_PA3/Main/com/tarney/main/MainStackDFS.java
@@ -0,0 +1,18 @@
+/*
+ * Main method which runs DFS using a stack and printing the edges
+ * 
+ * @author Brandon Tarney 4/28/2016
+ * @version 1.0
+ */
+
+package com.tarney.main;
+
+public class MainStackDFS {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Is this thing on?");
+
+	}
+
+}
