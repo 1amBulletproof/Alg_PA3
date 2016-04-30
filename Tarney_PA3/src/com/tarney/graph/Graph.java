df56427aadca8b97fc8ff8c2f9e96dda32b3040f
1amBulletproof <brandon.tarney@gmail.com>
2016-04-30 15:24:13 -0400

    Graph class made

---
+++ Tarney_PA3/src/com/tarney/graph/Graph.java
@@ -0,0 +1,12 @@
+/*
+ * Generic directed graph made up of vertexes and edges
+ * 
+ * @author Brandon Tarney 4/28/2016
+ * @version 1.0
+ */
+
+package com.tarney.graph;
+
+public class Graph {
+
+}
