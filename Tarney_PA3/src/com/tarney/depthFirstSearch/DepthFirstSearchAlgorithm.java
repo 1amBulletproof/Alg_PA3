ce87e8aae9e69da34028b7a008cabd26403de514
1amBulletproof <brandon.tarney@gmail.com>
2016-05-01 15:53:04 -0400

    +/-print all edges (even those which aren't a tree edge)

--- Tarney_PA3/src/com/tarney/depthFirstSearch/DepthFirstSearchAlgorithm.java
+++ Tarney_PA3/src/com/tarney/depthFirstSearch/DepthFirstSearchAlgorithm.java
@@ -15,25 +15,41 @@ import com.tarney.graph.Vertex;
 public class DepthFirstSearchAlgorithm {
 
 	/*
-	 * performs depth first search using a stack
+	 * performs depth first search using a stack, starting from the first vertex in the graph ArrayList
 	 * 
 	 * @param	graph	ArrayList of vertexes: essentially a graph
 	 * @return			returns the time the method took
 	 */
 	public static long depthFirstSearch(ArrayList<Vertex> graph) {
+		
 		long start = System.nanoTime();
 		Stack<Vertex> stack = new Stack<>();
 		
 		for (Vertex vertex: graph) {
-			vertex.setColor("white"); //no vertex has been visited yet
+			vertex.setColor("white");
+			vertex.newParents();
 		}
 		
+		Vertex childVertex;
+		
 		for ( Vertex vertex: graph) {
+			
 			if (vertex.getColor().equals("white")) {
-				stack = depthFirstSearchVisit(graph, vertex, stack);
+				System.out.println("visiting: " + vertex.getValue());
+				stack = depthFirstSearchVisit( vertex, stack);
+				
 				while (!stack.empty()) {
-					vertex = stack.pop();
-					stack = depthFirstSearchVisit(graph, vertex, stack);
+					childVertex = stack.pop();
+					
+					if (childVertex.getColor().equals("white")) {
+						System.out.println(displayEdge(childVertex.getLastParent(), childVertex) + " is a tree edge");
+						System.out.println("visiting: " + childVertex.getValue());
+						stack = depthFirstSearchVisit( childVertex, stack);
+					} 
+					else if (childVertex.getColor().equals("black")) {
+						System.out.println(displayEdge(childVertex.getLastParent(), childVertex) + " Ether a forward edge or a cross edge");
+					}
+
 				}
 			}
 		}
@@ -46,28 +62,32 @@ public class DepthFirstSearchAlgorithm {
 	/*
 	 * Visits a vertex in a graph while performing DFS
 	 * 
+	 * <p> a white node hasn't been visisted, a grey node has been visited but not all of its adjacent nodes have been visited 
+	 * and a black node has been visisted and all of its adjacent nodes has been visisted.
+	 * 
 	 * @param	graph	ArrayList of vertexes: essentially a graph
 	 * @param	vertex	vertex in graph which you wish to visit
 	 * @param	stack	stack of vertexes yet to be visited
 	 * @return			altered version of the input stack: there may be more vertexes added
 	 */
-	public static Stack<Vertex> depthFirstSearchVisit(ArrayList<Vertex> graph, Vertex vertex, Stack<Vertex> stackInput) {
+	public static Stack<Vertex> depthFirstSearchVisit(Vertex vertex, Stack<Vertex> stackInput) {
 		Stack<Vertex> stack = stackInput;
-		vertex.setColor("grey"); //the vertex has been visited
+		vertex.setColor("grey"); 
 		for (Vertex node: vertex.getAdjacencyList()) {
-			if (node.getColor().equals("white")) { //has the node been visited?
-				System.out.println(displayEdge(vertex, node) + " is a tree edge");
-				stack.push(node); //node hasn't been visited, add it to the stack
+			if (node.getColor().equals("white")) { 
+				node.addParent(vertex);
+				stack.push(node); 
 			}
 			else if (node.getColor().equals("grey")) { 
-				System.out.println(displayEdge(vertex, node) + " is a back edge");
-				
+				node.addParent(vertex);
+				System.out.println(displayEdge(node.getLastParent(), node) + " is a back edge");	
 			}
 			else {
-				System.out.println(displayEdge(vertex, node) + " Ether a forward edge or a cross edge");
+				node.addParent(vertex);
+				System.out.println(displayEdge(node.getLastParent(), node) + " Ether a forward edge or a cross edge");
 			}
 		}
-		vertex.setColor("black"); //node has been visited as has all its adjacent nodes
+		vertex.setColor("black");
 		return stack;
 	}
 	
